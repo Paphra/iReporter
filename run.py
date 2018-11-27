2ded81358718c525bea50e01f619e6eecdcd0a69
= <paphra.me@gmail.com>
2018-11-27 13:16:00 +0300

    Finished The endpoint of getting all the red flags To access this, the request should be having a json obejct indicating the user id so as to filter out the red flags for him or her. If an admin's id is included in the request json data, then all the flags are returned as a response to the request. If the id of an ordinary user is sent, the flags sent are for that user. No usr is able to see other flags except the admin [Finishes Delivers #162192361]

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, Response, url_for, jsonify
+import datetime
+import json
 
 app = Flask(__name__, template_folder='/UI/', static_folder='/UI/assets/')
 
@@ -41,7 +43,7 @@ def post_red_flag():
     except:
         res = {
             "status": 400,
-            "error": "No json object passed"
+            "error": "json object error"
         }
         return (jsonify(res), 400)
     
@@ -55,10 +57,109 @@ def flag_exists(title, comment):
 def add_new_flag(data):
     all_flags.append(data)
 
-all_flags = [
+all_flags = []
 
+all_users = [
+    {
+        "id": 3324332234,
+        "username": "Paphra",
+        "isAdmin": False,
+        "email": "paphra.me@gmail.com",
+        "phoneNumber": "0701822382",
+        "firstname": "Epaphradito",
+        "lastname": "Lugayavu",
+        "othernames": "Permutit",
+        "password": "123456789",
+        "registered": datetime.date.today().isoformat(),
+        "gender": "Male",
+        "occupation": "Software Developer",
+        "address": "Nalugala, Wakiso, Uganda"
+    },
+    {
+        "id": 2324331223,
+        "username": "Jules",
+        "isAdmin": False,
+        "email": "julius234@gmail.com",
+        "phoneNumber": "0793424212",
+        "firstname": "Julius",
+        "lastname": "Katamba",
+        "othernames": "",
+        "password": "223456789",
+        "registered": datetime.date.today().isoformat(),
+        "gender": "Male",
+        "occupation": "Farmer",
+        "address": "Lwatu, Nakasongola, Uganda"
+    },
+    {
+        "id": 2324332234,
+        "username": "RechealK",
+        "isAdmin": False,
+        "email": "rechealk2018@gmail.com",
+        "phoneNumber": "075900123",
+        "firstname": "Recheal",
+        "lastname": "Atuhaire",
+        "othernames": "Kunihira",
+        "password": "323456789",
+        "registered": datetime.date.today().isoformat(),
+        "gender": "Female",
+        "occupation": "Business Woman - Salon operator",
+        "address": "Lubaga, Kampala, Uganda"
+    },
+    {
+        "id": 2322904328,
+        "username": "Government",
+        "isAdmin": True,
+        "email": "info.security@mia.go.ug",
+        "phoneNumber": "07777522214",
+        "firstname": "Adolf",
+        "lastname": "Mwesigye",
+        "othernames": "",
+        "password": "987654321",
+        "registered": datetime.date.today().isoformat(),
+        "gender": "Male",
+        "occupation": "Minister for defence",
+        "address": "Entebbe, Wakiso, Uganda"
+    }
 ]
 
+@app.route("/api/v1/red-flags", methods=["GET"])
+def get_all_flags():
+    try:
+        json_data = request.get_json()
+        user_id = json_data.get("userId")
+        data = get_flags(user_id)
+        res = {
+            "status": 200,
+            "data": data
+        }
+
+        return (jsonify(res), 200)
+    except:
+        res = {
+            "status": 400,
+            "error": "json object error"
+        }
+
+        return (jsonify(res), 400)
+
+def get_flags(user_id):
+    if user_is_admin(user_id):
+        return all_flags
+    else:
+
+        flags = []
+        for flag in all_flags:
+            if flag["createdBy"] == user_id:
+                flags.append(flag)
+
+        return flags
+
+def user_is_admin(user_id):
+    for user in all_users:
+        if user["id"] == user_id:
+            if user["isAdmin"]:
+                return True
+    return False
 
 if __name__ == '__main__':
     app.run()
--- test_run.py
+++ test_run.py
@@ -13,13 +13,74 @@ class RunTestCase(ut.TestCase):
             "comment": "please, help us. We are dying.",
             "type": "Red-Flag",
             "createdOn": datetime.date.today().isoformat(),
-            "createdBy": 2324332234,
+            "createdBy": 3324332234,
             "location": "0.3421, 32.2231",
             "status": "Draft",
             "Images": ["image1", "image2"],
             "Videos": ["video1", "video2"],
             "otherFiles": ["file1","file2"]
         }
+        self.test_get_flag_records_inter = {
+            "id": 23210,
+            "title": "Roads are not okay",
+            "comment": "The bridge from kitaga to nyamitobola is down",
+            "type": "Intervention",
+            "createdOn": datetime.date.today().isoformat(),
+            "createdBy": 3324332234,
+            "location": "2.4232, 33.2231",
+            "status": "Draft",
+            "Images": ["image1", "image2"],
+            "Videos": ["video1", "video2"],
+            "otherFiles": ["file1","file2"]
+        }
+
+        self.test_get_flag_records_under_inv = {
+            "id": 23212,
+            "title": "Thief or the SACCO money",
+            "comment": "There is someone stealing our sacco money",
+            "type": "Red-Flag",
+            "createdOn": datetime.date.today().isoformat(),
+            "createdBy": 3324332234,
+            "location": "2.4232, 33.2231",
+            "status": "Under Investigation",
+            "Images": ["image1", "image2"],
+            "Videos": ["video1", "video2"],
+            "otherFiles": ["file1","file2"]
+        }
+        self.test_get_flag_records_not_for_user = {
+            "id": 23239,
+            "title": "Our Village is under siege",
+            "comment": "We have corrupt official on ground",
+            "type": "Red-Flag",
+            "createdOn": datetime.date.today().isoformat(),
+            "createdBy": 2324332223,
+            "location": "1.4232, 23.2231",
+            "status": "Under Investigation",
+            "Images": ["image1", "image2"],
+            "Videos": ["video1", "video2"],
+            "otherFiles": ["file1","file2"]
+        }
+
+        self.test_get_flag_records_resolved_not_for_user = {
+            "id": 23230,
+            "title": "The crops are dying off",
+            "comment": "We need some water for irrigations",
+            "type": "Red-Flag",
+            "createdOn": datetime.date.today().isoformat(),
+            "createdBy": 2324331223,
+            "location": "3.4232, 30.2231",
+            "status": "Resolved",
+            "Images": ["image1", "image2"],
+            "Videos": ["video1", "video2"],
+            "otherFiles": ["file1","file2"]
+        }
+
+        self.successful_create = {"data":[{"id":23219,"message":"Created a red-flag record"}],"status":201}
+        self.successful_create2 = {"data":[{"id":23210,"message":"Created a red-flag record"}],"status":201}
+        self.successful_create3 = {"data":[{"id":23212,"message":"Created a red-flag record"}],"status":201}
+        self.successful_create4 = {"data":[{"id":23239,"message":"Created a red-flag record"}],"status":201}
+        self.successful_create5 = {"data": [{"id": 23230, "message": "Created a red-flag record"}], "status": 201}
+        
 
     def tearDown(self):
         pass
@@ -35,22 +96,60 @@ class RunTestCase(ut.TestCase):
         with app.test_client() as c:
             
             rv = c.post("/api/v1/red-flags", json=self.test_new_flag_record_data)
+            rv2 = c.post("/api/v1/red-flags", json=self.test_get_flag_records_inter)
+            rv3 = c.post("/api/v1/red-flags", json=self.test_get_flag_records_under_inv)
+            rv4 = c.post("/api/v1/red-flags", json=self.test_get_flag_records_not_for_user)
+            rv5 = c.post("/api/v1/red-flags", json=self.test_get_flag_records_resolved_not_for_user)
 
-            assert Response.get_data(rv) == b'{"data":[{"id":23219,"message":"Created a red-flag record"}],"status":201}\n'
+            assert Response.get_json(rv) == self.successful_create
+            assert Response.get_json(rv2) == self.successful_create2
+            assert Response.get_json(rv3) == self.successful_create3
+            assert Response.get_json(rv4) == self.successful_create4
+            assert Response.get_json(rv5) == self.successful_create5
 
     # test repeated data
     def test_create_red_flag_record_data_repeat(self):
         with app.test_client() as d:
             rv2 = d.post("/api/v1/red-flags", json=self.test_new_flag_record_data)
 
-            assert Response.get_data(rv2) == b'{"data":[{"id":23219,"message":"Flag Already reported!"}],"status":200}\n'
+            assert Response.get_json(rv2) == {"data":[{"id":23219,"message":"Flag Already reported!"}],"status":200}
 
     def test_create_red_flag_record_no_data(self):
         with app.test_client() as d:
             rv2 = d.post("/api/v1/red-flags", json=None)
 
-            assert Response.get_data(rv2) == b'{"error":"No json object passed","status":400}\n'
+            assert Response.get_json(rv2) == {"error":"json object error","status":400}
 
+    def test_get_all_red_flags_call(self):
+        with app.test_client() as c:
+            rv = c.get("/api/v1/red-flags")
+            assert request.method == 'GET'
+            assert request.path == "/api/v1/red-flags"
+            assert request.get_json() == None
+
+    def test_get_all_red_flags_ordinary_user(self):
+        with app.test_client() as c:
+            
+            rv = c.get("/api/v1/red-flags", json={"userId": 2324332223})
+            rv2 = c.get("/api/v1/red-flags", json={"userId": 2324331223})
+            rv3 = c.get("/api/v1/red-flags", json={"userId": 3324332234})
+            
+            assert len(Response.get_json(rv)['data']) == 1
+            assert len(Response.get_json(rv2)['data']) == 1
+            assert len(Response.get_json(rv3)['data']) == 3
+
+            
+    def test_get_all_red_flags_admin(self):
+        with app.test_client() as c:
+            rv0 = c.get("/api/v1/red-flags", json={"userId": 2322904328})  # passed the admin id
+            assert len(Response.get_json(rv0)['data']) == 5
+            
+
+    def test_get_all_red_flags_no_json(self):
+        with app.test_client() as c:
+            rv = c.get("/api/v1/red-flags")
+            assert Response.get_json(rv) == {"error": "json object error", "status": 400}
+    
 
 if __name__ == '__main__':
     ut.main()
